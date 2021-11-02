d4385d39b001e2226ba06c53abfb092b155c5a97
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2021-11-02 16:36:56 -0600

    player class created and draw

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+const centerWidth = canvas.width/2;
+const centerHeight = canvas.height/2;
+
 //Classes creation: Background, Player and Enemies
 class Background{
     constructor(w,h){
@@ -21,13 +24,22 @@ class Background{
 class Player {
     constructor(x,y,w,h){
         this.x = x;
-        this.y = y
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.img = new Image()
+        this.img.src = "../images/sun.png"
     }
+    draw(){
+        ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    }
+
 }
 
 //------------------------------------------------------------------------//
 //CLASSES INHERETANCE
 const bg = new Background();
+const player = new Player(centerWidth,centerHeight,100,100);
 
 
 //------------------------------------------------------------------------//
@@ -36,6 +48,7 @@ function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0,0,canvas.width,canvas.height)
     bg.draw();
+    player.draw();
 }
 
 animate()

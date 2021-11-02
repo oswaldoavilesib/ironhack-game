d6c0be4a097dafcab98e4b0b996ad335cdc31169
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2021-11-02 16:27:37 -0600

    background class and draw done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
+//------------------------------------------------------------------------//
+//DEFAULT VARIABLES
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
 //Classes creation: Background, Player and Enemies
-
-
 class Background{
-    
+    constructor(w,h){
+        this.x = 0;
+        this.y = 0;
+        this.width = w;
+        this.height = h;
+        this.img = new Image()
+        this.img.src = "../images/space.jpg"
+    }
+    draw(){
+        ctx.drawImage(this.img,this.x,this.y,canvas.width,canvas.height)
+    }
 }
+
+class Player {
+    constructor(x,y,w,h){
+        this.x = x;
+        this.y = y
+    }
+}
+
+//------------------------------------------------------------------------//
+//CLASSES INHERETANCE
+const bg = new Background();
+
+
+//------------------------------------------------------------------------//
+//SCRIPTS//
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    bg.draw();
+}
+
+animate()
